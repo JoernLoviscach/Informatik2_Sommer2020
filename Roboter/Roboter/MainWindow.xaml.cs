5df3505d97a723ed5530f3ba980fed4e4a7cae8a
Jörn Loviscach <jl@j3L7h.de>
2020-04-30 16:42:59 +0200

    Stand nach der Veranstaltung

---
+++ Roboter/Roboter/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Roboter
+{
+    public partial class MainWindow : Window
+    {
+        SuperDuperRoboter robbi = new SuperDuperRoboter(3, 5);
+        SuperDuperRoboter robbi2 = new SuperDuperRoboter(4, 2);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GebeKoordinatenAus()
+        {
+            textBlockAusgabe.Text = "x1=" + robbi.X + "; y1=" + robbi.Y + "; x2=" + robbi2.X + "; y2=" + robbi2.Y;
+        }
+
+        private void Button_Click_Nr1Runter(object sender, RoutedEventArgs e)
+        {
+            robbi.BewegeNachUnten();
+            GebeKoordinatenAus();
+        }
+
+        private void Button_ClickNr1Rechts(object sender, RoutedEventArgs e)
+        {
+            robbi.BewegeNachRechts();
+            GebeKoordinatenAus();
+        }
+
+        private void Button_ClickNr2Runter(object sender, RoutedEventArgs e)
+        {
+            robbi2.BewegeNachUnten();
+            GebeKoordinatenAus();
+        }
+
+        private void Button_ClickNr2Rechts(object sender, RoutedEventArgs e)
+        {
+            robbi2.BewegeNachRechts();
+            GebeKoordinatenAus();
+        }
+    }
+
+    class SuperDuperRoboter // Vorsicht: Klassenname nicht wie Namespace-Name
+    {
+        // Attribute (Microsoft: "Felder"):
+        int x;
+        int y; // y-Achse zeigt nach unten!
+
+        // Properties = Eigenschaften:
+        public int X
+        {
+            get { return x; } // nur get, also nur lesbar
+        }
+        public int Y
+        {
+            get { return y; } // nur get, also nur lesbar
+        }
+
+        // Methoden:
+        // Constructor
+        public SuperDuperRoboter(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        // bewege den Roboter einen Schritt nach rechts
+        public void BewegeNachRechts()
+        {
+            x++;
+        }
+        // bewege den Roboter einen Schritt nach unten
+        public void BewegeNachUnten()
+        {
+            y++;
+        }
+    }
+}
---
+++ Roboter/Roboter2/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Roboter2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Baue einen Roboter
+            //Roboter 机械人 = new Roboter();
+
+            ////und bewege ihn nach rechts und nach unten
+            //机械人.BewegeNachRechts();
+            //机械人.BewegeNachUnten();
+
+        }
+    }
+
+    class Roboter
+    {
+        // Attribute (Microsoft: "Felder"):
+        int x;
+        int y; // y-Achse zeigt nach unten!
+
+        // Methoden:
+        // bewege den Roboter einen Schritt nach rechts
+        public void BewegeNachRechts()
+        {
+            x++;
+        }
+        // bewege den Roboter einen Schritt nach unten
+        public void BewegeNachUnten()
+        {
+            y++;
+        }
+
+
+    }
+}
